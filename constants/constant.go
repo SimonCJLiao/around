cf14ea0bd61ab87475fe20e24a70e7cf3b5fbbff
SimonCJLiao <975225926@qq.com>
2022-10-23 02:32:31 -0400

    initial commit

---
+++ constants/constant.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://34.171.171.238:9200"
+	ES_USERNAME = "simon"
+	ES_PASSWORD = "980916Jj"
+	GCS_BUCKET  = "around98"
+)
